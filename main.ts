a08e69513f1f9a8239ac02b0861bad0273ec173e
m-13756196-lab <m-13756196@moe-dl.edu.my>
2026-08-13 12:54:45 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        pins.servoWritePin(AnalogPin.P2, 0)
+    for (let index = 0; index < 8; index++) {
+        pins.servoWritePin(AnalogPin.P1, 90)
         basic.pause(500)
-        pins.servoWritePin(AnalogPin.P2, 40)
-        basic.pause(500)
+        pins.servoWritePin(AnalogPin.P1, 180)
+        basic.pause(2000)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Wedding), music.PlaybackMode.InBackground)
 })
 pins.digitalWritePin(DigitalPin.P0, 0)
 basic.showString("Hello!")
